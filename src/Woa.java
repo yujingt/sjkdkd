1469e9ec1cd41c629365fb04ca1b0f4aa207a700
yujingt <yjttxws@outlook.com>
2019-10-13 12:07:39 +0800

    第二次

---
+++ src/Woa.java
@@ -0,0 +1,7 @@
+public class Woa {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("my god");
+    }
+}
